79fa82eec563f4d62503a10662e6c10ec043cd7a
Kirill Murzin <murz.kirill@yandex.ru>
2022-11-10 02:55:02 +0500

    total_amount

--- frontend/static/scripts/main.js
+++ frontend/static/scripts/main.js
@@ -1,10 +1,31 @@
+currency_types = {
+    'RUB': 'руб.',
+    'USD': 'дол.',
+    'EUR': 'евр.'
+}
+
 function get_accounts(user_id) {
     $.ajax({
         type: "POST",
         url: "http://localhost:5000/get_accounts",
         data: JSON.stringify({user_id: 1}),
         success: (data) => {
-            console.log(data)
+            let total_balance = {}
+
+            for (let currency in currency_types) {
+                total_balance[currency] = 0;
+            }
+
+            for (let i of Object.values(data)) {
+                for (let account of i.accounts) {
+                    for (let balance of account.balance.Data.Balance) {
+                        total_balance[balance.Amount.currency] += balance.Amount.amount
+                    }
+                }
+            }
+            for (let currency in total_balance) {
+                $(`#${currency}_balance`).text(total_balance[currency].toString() + ' ' + currency_types[currency])
+            }
         },
         dataType: "json",
         contentType: "application/json; charset=utf-8"
