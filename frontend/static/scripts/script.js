d7c1c8f15756524e6da3377f422beb77f057f8f0
Roman <nash.roma37@gmail.com>
2022-11-09 20:54:48 +0500

    front upload

---
+++ frontend/static/scripts/script.js
@@ -0,0 +1,12 @@
+
+
+function openMainPage() {
+    location.href="http://127.0.0.1:5500/finnopolis-case/frontend/html/main.html";
+}
+
+
+
+
+function openAccountPage() {
+    location.href="http://127.0.0.1:5500/finnopolis-case/frontend/html/transactions.html";
+} 
