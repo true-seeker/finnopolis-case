2c7e9c258c2c300d869b33f0b4f4a72616bc8555
Kirill Murzin <murz.kirill@yandex.ru>
2022-11-09 18:49:12 +0500

    get_accounts

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,75 @@
-from flask import Flask
+from dataclasses import dataclass
+
+import requests
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
-from controllers import app
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+db = SQLAlchemy(app)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    accounts = relationship("Account", backref='users', cascade="all, delete, delete-orphan")
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class Bank(db.Model):
+    __tablename__ = 'banks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), unique=True, nullable=False)
+    accounts = relationship("Account", backref='banks', cascade="all, delete, delete-orphan")
+    application_uri = db.Column(db.String(255), unique=True, nullable=False)
+
+
+@dataclass
+class Account(db.Model):
+    __tablename__ = 'accounts'
+    id: int
+    user_id: int
+    account_id: int
+    bank_id: int
+    name: str
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'))
+    account_id = db.Column(db.String(255), unique=True, nullable=False)
+    bank_id = Column(Integer, ForeignKey('banks.id', ondelete='CASCADE'))
+    name = db.Column(db.String(255), unique=True, nullable=False)
+
+
+@app.route("/get_accounts", methods=['POST'])
+def get_accounts():
+    js = request.json
+    user_id = js['user_id']
+    user = db.get_or_404(User, user_id)
+    user_accounts = user.accounts
+
+    result = {}
+    for account in user_accounts:
+        bank = db.get_or_404(Bank, account.bank_id)
+        if result.get(bank.id) is None:
+            result[bank.id] = {'bank_name': bank.name,
+                               'accounts': []}
+
+        try:
+            r = requests.get(f'{bank.application_uri}open-banking/v1.3/aisp/accounts/{account.account_id}')
+        except requests.exceptions.ConnectionError:
+            continue
+
+        if r.status_code == 200:
+            result[bank.id]['accounts'].append(r.json())
+
+    return result
 
 
 if __name__ == '__main__':
--- backend/models.py
+++ backend/models.py
@@ -1,39 +1,49 @@
-from sqlalchemy import Column, Integer, ForeignKey
-from sqlalchemy.orm import relationship
-
-from controllers import db, app
+from backend.main import app, Bank, User, Account, db
 
 
-class User(db.Model):
-    __tablename__ = 'users'
+def generate_data():
+    bank1 = Bank(name="Зеленый банк", application_uri="http://localhost:5466/")
+    bank2 = Bank(name="Желтый банк", application_uri="http://localhost:5467/")
+    db.session.add(bank1)
+    db.session.add(bank2)
+    db.session.commit()
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255), unique=True, nullable=False)
-    accounts = relationship("Account", backref='users', cascade="all, delete, delete-orphan")
+    user1 = User(username='user1')
+    user2 = User(username='user2')
+    user3 = User(username='user3')
+    db.session.add(user1)
+    db.session.add(user2)
+    db.session.add(user3)
+    db.session.commit()
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    account1 = Account(user_id=user1.id, account_id='1002001', bank_id=bank1.id, name='Счёт №1')
+    account2 = Account(user_id=user1.id, account_id='1002002', bank_id=bank1.id, name='Счёт №2')
+    account3 = Account(user_id=user1.id, account_id='3589438', bank_id=bank1.id, name='Счёт №3')
 
+    account4 = Account(user_id=user2.id, account_id='534434', bank_id=bank2.id, name='Счёт №4')
+    account5 = Account(user_id=user2.id, account_id='76578', bank_id=bank2.id, name='Счёт №5')
 
-class Bank(db.Model):
-    __tablename__ = 'banks'
+    account6 = Account(user_id=user3.id, account_id='12321', bank_id=bank1.id, name='Счёт №6')
+    account7 = Account(user_id=user3.id, account_id='86787', bank_id=bank1.id, name='Счёт №7')
+    account8 = Account(user_id=user3.id, account_id='45645', bank_id=bank2.id, name='Счёт №8')
+    account9 = Account(user_id=user3.id, account_id='123121', bank_id=bank2.id, name='Счёт №9')
+    account10 = Account(user_id=user3.id, account_id='123233', bank_id=bank2.id, name='Счёт №10')
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), unique=True, nullable=False)
-    accounts = relationship("Account", backref='banks', cascade="all, delete, delete-orphan")
-    application_uri = db.Column(db.String(255), unique=True, nullable=False)
-
-
-class Account(db.Model):
-    __tablename__ = 'accounts'
-
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'))
-    account_id = db.Column(db.String(255), unique=True, nullable=False)
-    bank_id = Column(Integer, ForeignKey('banks.id', ondelete='CASCADE'))
+    db.session.add(account1)
+    db.session.add(account2)
+    db.session.add(account3)
+    db.session.add(account4)
+    db.session.add(account5)
+    db.session.add(account6)
+    db.session.add(account7)
+    db.session.add(account8)
+    db.session.add(account9)
+    db.session.add(account10)
+    db.session.commit()
 
 
 if __name__ == '__main__':
     with app.app_context():
-        # db.drop_all()
+        db.drop_all()
         db.create_all()
+        generate_data()
